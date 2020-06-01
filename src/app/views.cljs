6a193f1e7f5db670345f4cb5f8572ed1db84d567
Alexander Crompton <alexcromp@gmail.com>
2020-06-01 21:54:23 +0200

    Refactor

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,8 +1,12 @@
 (ns app.views)
 
-(defn app
-  []
+(def make-alert #(.alert js/window "Button clicked"))
+
+(defn button [text make-alert]
+  [:button {:on-click #(.alert js/window "Button clicked")} text])
+
+(defn app []
   [:div
    [:h1 "Vocabulary Drills"]
-   [:button {:on-click #(.alert js/window "Button clicked")} "Start exercises"]
+   (button "Start exercises" make-alert)
    [:p "More to come"]])
