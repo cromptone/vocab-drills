a3451dc7843d564b11d18e795df19ad8a8e3ec74
Alexander Crompton <alexcromp@gmail.com>
2020-06-04 18:45:47 +0200

    Create function to see extensions of vocab files

--- src/app/compile_vocab.clj
+++ src/app/compile_vocab.clj
@@ -1,10 +1,12 @@
 (ns app.compile-vocab
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import  [org.apache.commons.io FilenameUtils]))
 
 (defn compile-vocab []
   (let [files (-> "./src/vocab" io/file .listFiles)
         reduce-file #(with-open [rdr (io/reader %)]
                        (reduce conj [] (line-seq rdr)))]
     (io/make-parents "./src/vocab/compiled_vocab.edn")
+    (prn (map #(FilenameUtils/getExtension (.getName %)) files))
     (with-open [wrtr (io/writer "./src/vocab/compiled_vocab.edn")]
       (.write wrtr (pr-str (flatten (map reduce-file files)))))))
