96cb527490556d8aa3f0a33529283e52aff8fccf
Alexander Crompton <alexcromp@gmail.com>
2020-06-07 12:20:35 +0200

    Filter so only txt files get parsed

--- src/app/compile_vocab.clj
+++ src/app/compile_vocab.clj
@@ -3,10 +3,10 @@
   (:import  [org.apache.commons.io FilenameUtils]))
 
 (defn compile-vocab []
-  (let [files (-> "./src/vocab" io/file .listFiles)
+  (let [txt-file? #(= "txt" (-> % .getName FilenameUtils/getExtension))
+        files (->> "./src/vocab" io/file .listFiles (filter txt-file?))
         reduce-file #(with-open [rdr (io/reader %)]
                        (reduce conj [] (line-seq rdr)))]
     (io/make-parents "./src/vocab/compiled_vocab.edn")
-    (prn (map #(FilenameUtils/getExtension (.getName %)) files))
     (with-open [wrtr (io/writer "./src/vocab/compiled_vocab.edn")]
       (.write wrtr (pr-str (flatten (map reduce-file files)))))))
