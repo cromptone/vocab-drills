35225e006500ca73027bc06a2343595ae3081c89
Alexander Crompton <alexcromp@gmail.com>
2020-06-18 19:37:27 +0200

    Add click function with buttons

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -2,8 +2,11 @@
   (:require [shadow.resource :as rc]
             [clojure.edn :as edn]))
 
+(defn click-fn [title]
+  (js/alert title))
+
 (defn app []
   [:div
    [:h1 "Vocabulary Drills"]
-   (for [title (map :title (edn/read-string (rc/inline "../vocab/compiled_vocab.edn")))]
-     [:button {:key title} title])])
+   (for [vocab-list (edn/read-string (rc/inline "../vocab/compiled_vocab.edn"))]
+     [:button {:key (:title vocab-list) :on-Click #(click-fn (:vocab vocab-list))} (:title vocab-list)])])
