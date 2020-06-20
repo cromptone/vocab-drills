e101c86fdc4b63ed0a56bd2aa199545261af22e9
Alexander Crompton <alexcromp@gmail.com>
2020-06-20 10:46:03 +0200

    Make vocab compilation split ger/eng versions of vocab

--- src/app/compile_vocab.clj
+++ src/app/compile_vocab.clj
@@ -1,16 +1,23 @@
 (ns app.compile-vocab
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn])
+            [clojure.edn :as edn]
+            [clojure.string :as str])
   (:import  [org.apache.commons.io FilenameUtils]))
+
+(defn reduce-file [f]
+  (with-open [rdr (io/reader f)]
+    {:title (with-open [rdr (io/reader f)]
+              (first (line-seq rdr)))
+     :vocab (with-open [rdr (io/reader f)]
+              (map #(str/split % #"\t")
+                   (reduce
+                    conj
+                    []
+                    (rest (line-seq rdr)))))}))
 
 (defn compile-vocab []
   (let [txt-file? #(= "txt" (-> % .getName FilenameUtils/getExtension))
-        files (->> "./src/vocab" io/file .listFiles (filter txt-file?))
-        reduce-file #(with-open [rdr (io/reader %)]
-                       {:title (with-open [rdr (io/reader %)]
-                                 (first (line-seq rdr)))
-                        :vocab (with-open [rdr (io/reader %)]
-                                 (reduce conj [] (rest (line-seq rdr))))})]
+        files (->> "./src/vocab" io/file .listFiles (filter txt-file?))]
     (io/make-parents "./src/vocab/compiled_vocab.edn")
     (with-open [wrtr (io/writer "./src/vocab/compiled_vocab.edn")]
       (.write wrtr (pr-str (vec (flatten (map reduce-file files))))))
