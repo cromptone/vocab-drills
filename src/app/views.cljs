f6ee1b1357d2a1d399ac70befc43a51c53b02852
Alexander Crompton <alexcromp@gmail.com>
2020-06-22 22:22:23 +0200

    Use threading macro for readability

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -9,7 +9,10 @@
     [:button {:key title :on-click #(click-fn vocab)} title]))
 
 (defn exercise []
-  (for [[idx [ger eng]] (shuffle (map-indexed vector (:vocab @(rf/subscribe [:current-exercise]))))]
+  (for [[idx [ger eng]] (->> @(rf/subscribe [:current-exercise])
+                             :vocab
+                             (map-indexed vector)
+                             shuffle)]
     [:div.cloud-word {:key (str idx "-" eng)} (str idx ger)]))
 
 (defn app []
