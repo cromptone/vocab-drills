28409dc596e23e1b4b410833f7dc6fc39722f8a4
Alexander Crompton <alexcromp@gmail.com>
2020-07-08 19:13:48 +0200

    Refactor menu to use menu-items

--- src/app/menu.cljs
+++ src/app/menu.cljs
@@ -1,6 +1,20 @@
 (ns app.menu)
 
+(defn menu-item [{:keys [name href page]}]
+  [:a {:on-click (:set-page page)
+       :href href
+       :key name}
+   name])
+
+(def menu-items
+  [{:name "Exercises"
+    :href "/exercises"
+    :page :about}
+   {:name "About"
+    :href "/about"
+    :page :about}])
+
 (defn menu []
-  [:nav "Menu"
-   [:a {:href "/about"} "About"]
-   [:a {:href "/exercise"} "Exercise"]])
+  [:nav "German vocabulary drills"
+   (for [item menu-items]
+     (menu-item item))])
