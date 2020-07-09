9d48aaf055a2fe3f251259fefbbf426e9261fa02
Alexander Crompton <alexcromp@gmail.com>
2020-07-09 19:03:13 +0200

    Whitespace and linebreak (code beauty)

--- src/app/events.cljs
+++ src/app/events.cljs
@@ -2,10 +2,11 @@
   (:require [re-frame.core :as rf]))
 
 (defn get-unanswered [db] (get-in db [:exercise :vocab :unanswered]))
-(defn get-dropped [db] (get-in db [:exercise :vocab :dropped]))
-(defn get-answered [db] (get-in db [:exercise :vocab :answered]))
+(defn get-dropped    [db] (get-in db [:exercise :vocab :dropped]))
+(defn get-answered   [db] (get-in db [:exercise :vocab :answered]))
+
 (defn clear-answered [db] (assoc-in db [:exercise :vocab :answered] []))
-(defn clear-dropped [db] (assoc-in db [:exercise :vocab :dropped] []))
+(defn clear-dropped  [db] (assoc-in db [:exercise :vocab :dropped] []))
 
 (rf/reg-event-db
  :set-exercise-option
@@ -31,14 +32,11 @@
  (fn [db [_ _]]
    (let [answered (get-answered db)
          dropped (get-dropped db)]
-
      (-> db
          (clear-answered)
          (clear-dropped)
-         (update-in [:exercise :vocab :unanswered] #(->> %
-                                                         (concat answered
-                                                                 dropped)
-                                                         shuffle))))))
+         (update-in [:exercise :vocab :unanswered]
+                    #(->> % (concat answered dropped) shuffle))))))
 
 (rf/reg-event-db
  :move-vocab-status
