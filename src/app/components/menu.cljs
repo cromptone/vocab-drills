285d48b6d11df848436c73a305d3e222e52652b0
Alexander Crompton <alexcromp@gmail.com>
2020-07-09 20:42:16 +0200

    Code beauty

--- src/app/components/menu.cljs
+++ src/app/components/menu.cljs
@@ -1,15 +1,15 @@
 (ns app.components.menu
   (:require [app.router :as router]))
 
+(def menu-items
+  [{:name "Exercises" :page :exercise}
+   {:name "About" :page :about}])
+
 (defn menu-item [{:keys [name page]}]
   [:a {:key name
        :on-click (:set-page page)
        :href (router/router-path page)}
    name])
-
-(def menu-items
-  [{:name "Exercises" :page :exercise}
-   {:name "About" :page :about}])
 
 (defn menu []
   [:nav "German vocabulary drills"
