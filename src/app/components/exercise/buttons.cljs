aa753ca06ef09e3724c0c49c430d7a9a70237c2a
Alexander Crompton <alexcromp@gmail.com>
2020-07-09 21:49:54 +0200

    Conditionally disable remove-20 button

--- src/app/components/exercise/buttons.cljs
+++ src/app/components/exercise/buttons.cljs
@@ -1,16 +1,18 @@
 (ns app.components.exercise.buttons
   (:require [re-frame.core :as rf]))
 
-(def button-data
-  [{:text "Go back"
-    :dispatch-kw :clear-exercise}
+(defn button-data []
+  [{:text "Go back" :dispatch-kw :clear-exercise}
    {:text "Remove 20 words"
-    :dispatch-kw :remove-20-words}
-   {:text "Reset exercise"
-    :dispatch-kw :reset}])
+    :dispatch-kw :remove-20-words
+    :disabled (->> @(rf/subscribe [:unanswered-vocab])
+                   count
+                   (>= 20))}
+   {:text "Reset exercise" :dispatch-kw :reset}])
 
 (defn buttons []
-  (for [{:keys [dispatch-kw text]} button-data]
+  (for [{:keys [dispatch-kw text disabled]} (button-data)]
     [:button {:key text
-              :on-click #(rf/dispatch [dispatch-kw])}
+              :on-click #(rf/dispatch [dispatch-kw])
+              :disabled disabled}
      text]))
