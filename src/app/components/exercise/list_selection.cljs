578fce778cfee1f93ef53f7482c93257cc5bede5
Alexander Crompton <alexcromp@gmail.com>
2020-07-09 20:43:03 +0200

    Input actual exercise options

--- src/app/components/exercise/list_selection.cljs
+++ src/app/components/exercise/list_selection.cljs
@@ -7,12 +7,16 @@
               :on-click #(rf/dispatch [:set-vocab-list id])}
      title]))
 
+(def options-data
+  [{:name "Word cloud" :option-kw :word-cloud}
+   {:name "Single-word prompt" :option-kw :prompt}])
+
 (defn exercise-options []
   [:div
-   [:button {:on-click #(rf/dispatch [:set-exercise-option :a])}
-    "Exercise A"]
-   [:button {:on-click #(rf/dispatch [:set-exercise-option :b])}
-    "Exercise B"]])
+   (for [{:keys [name option-kw]} options-data]
+     [:button {:key name
+               :on-click #(rf/dispatch [:set-exercise-option option-kw])}
+      name])])
 
 (defn lists []
   [:<>
