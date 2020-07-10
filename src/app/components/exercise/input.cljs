839acf80fa519c1234e318c64c439e47b204a077
Alexander Crompton <alexcromp@gmail.com>
2020-07-10 09:28:35 +0200

    Only take inputs upon "Enter"

--- src/app/components/exercise/input.cljs
+++ src/app/components/exercise/input.cljs
@@ -2,13 +2,14 @@
   (:require [re-frame.core :as rf]))
 
 (defn input-handler [e]
-  (let [value (.. e -target -value)
-        vocab @(rf/subscribe [:correct-answers])
-        input-value-correct? (some #(= value %) (map first vocab))]
-    (when input-value-correct?
-      (do
-        (rf/dispatch [:move-vocab-status value])
-        (-> js/document (.getElementById "vocab-input") .-value (set! ""))))))
+  (when (= (.. e -keyCode) 13)
+    (let [value (.. e -target -value)
+          vocab @(rf/subscribe [:correct-answers])
+          input-value-correct? (some #(= value %) (map first vocab))]
+      (when input-value-correct?
+        (do
+          (rf/dispatch [:move-vocab-status value])
+          (-> js/document (.getElementById "vocab-input") .-value (set! "")))))))
 
 (defn input []
   [:input {:id "vocab-input"
