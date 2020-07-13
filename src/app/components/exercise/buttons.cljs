14d8d7d2e33630c1723cc05b59caf211cdedc1ca
Alexander Crompton <alexcromp@gmail.com>
2020-07-13 21:27:09 +0200

    Rename boolean disabled? to have question mark

--- src/app/components/exercise/buttons.cljs
+++ src/app/components/exercise/buttons.cljs
@@ -5,15 +5,14 @@
   [{:text "Go back" :dispatch-kw :clear-exercise}
    {:text "Remove 20 words"
     :dispatch-kw :remove-20-words
-    :disabled (->> @(rf/subscribe [:unanswered-vocab])
-                   count
-                   (>= 20))}
+    :disabled? (->> @(rf/subscribe [:unanswered-vocab]) count (>= 20))}
    {:text "Restart" :dispatch-kw :reset}
    {:text "Redo with missed" :dispatch-kw :redo-with-missed}
    {:text "Show answers" :dispatch-kw :show-answers}])
+
 (defn buttons []
-  (for [{:keys [dispatch-kw text disabled]} (button-data)]
+  (for [{:keys [dispatch-kw text disabled?]} (button-data)]
     [:button {:key text
               :on-click #(rf/dispatch [dispatch-kw])
-              :disabled disabled}
+              :disabled disabled?}
      text]))
