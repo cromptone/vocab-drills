1d7711550bb3a3fa927ea1f535a697402f42bc24
Alexander Crompton <alexcromp@gmail.com>
2020-07-15 15:05:28 +0200

    Refactor generating key

--- src/app/components/exercise/clouds.cljs
+++ src/app/components/exercise/clouds.cljs
@@ -1,6 +1,9 @@
 (ns app.components.exercise.clouds
   (:require [re-frame.core :as rf]))
 
+(defn gen-cloud-key-map [ger eng]
+  {:key (str ger "-" eng)})
+
 (defn example []
   (when (-> @(rf/subscribe [:answered-vocab]) count (< 3))
     [:div.cloud-word.cloud-word__example
@@ -9,15 +12,15 @@
 (defn unanswered []
   (when @(rf/subscribe [:show-unanswered-cloud?])
     (for [[ger eng] @(rf/subscribe [:unanswered-vocab])]
-      [:div.cloud-word.cloud-word__unanswered {:key (str ger "-" eng)}
+      [:div.cloud-word.cloud-word__unanswered (gen-cloud-key-map ger eng)
        (str eng)])))
 
 (defn answered []
   (for [[ger eng] @(rf/subscribe [:answered-vocab])]
-    [:div.cloud-word.cloud-word__answered {:key (str ger "-" eng)}
+    [:div.cloud-word.cloud-word__answered (gen-cloud-key-map ger eng)
      (str ger " → " eng)]))
 
 (defn incorrect []
   (for [[ger eng] @(rf/subscribe [:incorrect-vocab])]
-    [:div.cloud-word.cloud-word__incorrect {:key (str ger "-" eng)}
+    [:div.cloud-word.cloud-word__incorrect (gen-cloud-key-map ger eng)
      (str ger " → " eng)]))
