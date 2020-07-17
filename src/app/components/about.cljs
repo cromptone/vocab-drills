3e62e7b8557520c0db67f84884650f401d442d62
Alexander Crompton <alexcromp@gmail.com>
2020-07-17 21:26:36 +0200

    Give about page keys for the buttons

--- src/app/components/about.cljs
+++ src/app/components/about.cljs
@@ -10,9 +10,10 @@
    [:p "This is a German language website coded by Alexander Crompton. I created it because I wanted a customizable vocabulary drill app that matched my learning style; I also wanted to publish a website showing off my Clojure skills (since most of the Clojure work I've done so far is proprietary)."]
    [:p "This website is coded in ClojureScipt using Reagent and re-frame. The vocabulary is compiled from flat files using a Clojure script that runs at build-time. The CSS is written using the Garden library (though for a similar project in the future I'd probably stick to Sass), with the router being managed with the pushy library."]
    [:h2 "Want more vocabulary?"]
-   [:p "Adding vocabulary is easy! Feel free to reach out to me if you'd like to see more vocab, or if you have any more ideas of how this might be useful for you."]
+   [:p "Adding new vocabulary is easy! View the repository's read-me for more information. (Or contact me directly!)"]
    (for [{:keys [href text]} buttons]
      [:a {:type "button"
           :href href
+          :key text
           :target "_blank"}
       [:button.big-btn text]])])
